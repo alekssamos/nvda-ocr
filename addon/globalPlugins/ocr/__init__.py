9f33fc0e93a4737e26d40f774f45d4afc73be432
alekssamos <aleks-samos@yandex.ru>
2020-03-16 00:24:03 +0300

    Add Multi-Language Support. Select in OCR settings.

--- addon/globalPlugins/ocr/__init__.py
+++ addon/globalPlugins/ocr/__init__.py
@@ -36,91 +36,6 @@ TESSERACT_EXE = os.path.join(PLUGIN_DIR, "tesseract", "tesseract.exe")
 
 IMAGE_RESIZE_FACTOR = 2
 
-class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-	def __init__(self):
-		super(globalPluginHandler.GlobalPlugin, self).__init__()
-		self.ocrSettingsItem = gui.mainFrame.sysTrayIcon.preferencesMenu.Append(wx.ID_ANY,
-			# Translators: The name of the OCR settings item
-			# in the NVDA Preferences menu.
-			_("OCR settings..."))
-		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, self.onOCRSettings, self.ocrSettingsItem)
-
-	def terminate(self):
-		try:
-			gui.mainFrame.sysTrayIcon.preferencesMenu.RemoveItem(self.ocrSettingsItem)
-		except wx.PyDeadObjectError:
-			pass
-
-	def onOCRSettings(self, event):
-		# Pop a dialog with available OCR languages to set
-		langs = sorted(getAvailableTesseractLanguages())
-		curlang = getConfig()['language']
-		try:
-			select = langs.index(curlang)
-		except ValueError:
-			select = langs.index('eng')
-		choices = [languageHandler.getLanguageDescription(tesseractLangsToLocales[lang]) or tesseractLangsToLocales[lang] for lang in langs]
-		log.debug("Available OCR languages: %s", ", ".join(choices))
-		dialog = wx.SingleChoiceDialog(gui.mainFrame, _("Select OCR Language"), _("OCR Settings"), choices=choices)
-		dialog.SetSelection(select)
-		gui.mainFrame.prePopup()
-		ret = dialog.ShowModal()
-		gui.mainFrame.postPopup()
-		if ret == wx.ID_OK:
-			lang = langs[dialog.GetSelection()]
-			getConfig()['language'] = lang
-			try:
-				getConfig().write()
-			except IOError:
-				log.error("Error writing ocr configuration", exc_info=True)
-	def script_ocrNavigatorObject(self, gesture):
-		nav = api.getNavigatorObject()
-		left, top, width, height = nav.location
-		bmp = wx.EmptyBitmap(width, height)
-		# Tesseract copes better if we convert to black and white...
-		## img = img.convert(mode='L')
-		# and increase the size.
-		## img = img.resize((width * IMAGE_RESIZE_FACTOR, height * IMAGE_RESIZE_FACTOR), Image.BICUBIC)
-		mem = wx.MemoryDC(bmp)
-		mem.Blit(0, 0, width, height, wx.ScreenDC(), left, top)
-		
-		baseFile = os.path.join(tempfile.gettempdir(), "nvda_ocr")
-		try:
-			imgFile = baseFile + ".png"
-			txtFile = baseFile + ".txt"
-			bmp.SaveFile(imgFile, wx.BITMAP_TYPE_PNG)
-			
-			ui.message(_("Running OCR"))
-			lang = getConfig()['language']
-			# Hide the Tesseract window.
-			si = subprocess.STARTUPINFO()
-			si.dwFlags = subprocess.STARTF_USESHOWWINDOW
-			si.wShowWindow = subprocess.SW_HIDE
-			subprocess.check_call((TESSERACT_EXE, imgFile, baseFile, "-l", lang),
-				startupinfo=si)
-		finally:
-			try:
-				os.remove(imgFile)
-			except OSError:
-				pass
-		# Let the user review the OCR output.
-		result = ""
-		try:
-			with open(txtFile, encoding="UTF-8") as f: result = f.read().strip()
-		except: pass
-		os.remove(txtFile)
-		if result != "":
-			ui.message(_("Done"))
-			try:
-				ui.browseableMessage(result, _("OCR Result"))
-			except:
-				ui.message(result)
-		else:
-			ui.message(_("Error"))
-	script_ocrNavigatorObject.__doc__ = _('Recognize text use tesseract.')
-	__gestures = {
-		"kb:NVDA+r": "ocrNavigatorObject",
-	}
 
 localesToTesseractLangs = {
 "bg" : "bul",
@@ -169,9 +84,122 @@ def getDefaultLanguage():
 		lang = lang.split("_")[0]
 	return localesToTesseractLangs.get(lang, "eng")
 
+class SettingsDialog(gui.SettingsDialog):
+	# Pop a dialog with available OCR languages to set
+	title = _("OCR Settings")
+	tlangs = getAvailableTesseractLanguages()
+	
+	def makeSettings(self, sizer):
+		settingsSizerHelper = gui.guiHelper.BoxSizerHelper(self, sizer=sizer)
+
+		langs = sorted(self.tlangs)
+		choices = [languageHandler.getLanguageDescription(tesseractLangsToLocales[lang]) or
+			tesseractLangsToLocales[lang] for lang in langs]
+		log.debug("Available OCR languages: %s", ", ".join(choices))
+		
+		curlang = getConfig()['language']
+		try:
+			select = langs.index(curlang)
+		except ValueError:
+			select = langs.index('en')
+		self.language = settingsSizerHelper.addLabeledControl(_("Select OCR Language"), wx.Choice, choices=choices)
+		self.language.SetSelection(select)
+		
+		curlang2 = getConfig()['language2']
+		try:
+			select2 = langs.index(curlang2)
+		except ValueError:
+			select2 = langs.index('en')
+		self.language2 = settingsSizerHelper.addLabeledControl(_("Select second OCR Language"), wx.Choice, choices=choices)
+		self.language2.SetSelection(select2)
+	
+	def postInit(self):
+		self.language.SetFocus()
+
+	def onOk(self, event):
+		getConfig()["language"] = self.tlangs[self.language.GetSelection()]
+		getConfig()["language2"] = self.tlangs[self.language2.GetSelection()]
+
+		try:
+			getConfig().write()
+		except IOError:
+			log.error("Error writing ocr configuration", exc_info=True)
+		super(SettingsDialog, self).onOk(event)
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	def __init__(self):
+		super(globalPluginHandler.GlobalPlugin, self).__init__()
+		self.ocrSettingsItem = gui.mainFrame.sysTrayIcon.preferencesMenu.Append(wx.ID_ANY,
+			# Translators: The name of the OCR settings item
+			# in the NVDA Preferences menu.
+			_("OCR settings..."))
+		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, lambda evt: gui.mainFrame._popupSettingsDialog(SettingsDialog), self.ocrSettingsItem)
+
+	def terminate(self):
+		try:
+			gui.mainFrame.sysTrayIcon.preferencesMenu.RemoveItem(self.ocrSettingsItem)
+		except wx.PyDeadObjectError:
+			pass
+
+	def script_ocrNavigatorObject(self, gesture):
+		nav = api.getNavigatorObject()
+		left, top, width, height = nav.location
+		bmp = wx.EmptyBitmap(width, height)
+		# Tesseract copes better if we convert to black and white...
+		## img = img.convert(mode='L')
+		# and increase the size.
+		## img = img.resize((width * IMAGE_RESIZE_FACTOR, height * IMAGE_RESIZE_FACTOR), Image.BICUBIC)
+		mem = wx.MemoryDC(bmp)
+		mem.Blit(0, 0, width, height, wx.ScreenDC(), left, top)
+		
+		baseFile = os.path.join(tempfile.gettempdir(), "nvda_ocr")
+		try:
+			imgFile = baseFile + ".png"
+			txtFile = baseFile + ".txt"
+			bmp.SaveFile(imgFile, wx.BITMAP_TYPE_PNG)
+			
+			ui.message(_("Running OCR"))
+			lang = getConfig()['language']
+			lang2 = getConfig()['language2']
+			if not lang == lang2:
+				mlangs = lang + "+" + lang2
+			else:
+				mlangs = lang
+			# Hide the Tesseract window.
+			si = subprocess.STARTUPINFO()
+			si.dwFlags = subprocess.STARTF_USESHOWWINDOW
+			si.wShowWindow = subprocess.SW_HIDE
+			subprocess.check_call((TESSERACT_EXE, imgFile, baseFile, "-l", mlangs),
+				startupinfo=si)
+		finally:
+			try:
+				os.remove(imgFile)
+			except OSError:
+				pass
+		# Let the user review the OCR output.
+		result = ""
+		try:
+			with open(txtFile, encoding="UTF-8") as f: result = f.read().strip()
+		except: pass
+		os.remove(txtFile)
+		if result != "":
+			ui.message(_("Done"))
+			try:
+				ui.browseableMessage(result, _("OCR Result"))
+			except:
+				ui.message(result)
+		else:
+			ui.message(_("Error"))
+	script_ocrNavigatorObject.__doc__ = _('Recognize text use tesseract.')
+	__gestures = {
+		"kb:NVDA+r": "ocrNavigatorObject",
+	}
+
+
 _config = None
 configspec = StringIO("""
 language=string(default={defaultLanguage})
+language2=string(default={defaultLanguage})
 """.format(defaultLanguage=getDefaultLanguage()))
 def getConfig():
 	global _config
